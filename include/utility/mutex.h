e40646ce2425b4b2c43f6055847f09096a781756
wangxinshuo <xinshuowang@163.com>
2021-03-13 14:18:44 +0800

    add low version mutex implementation.

--- include/utility/mutex.h
+++ include/utility/mutex.h
@@ -5,6 +5,8 @@
 #ifndef UTILITY_INCLUDE_UTILITY_MUTEX_H_
 #define UTILITY_INCLUDE_UTILITY_MUTEX_H_
 
+#if __cplusplus >= 199711L
+
 #include <mutex>
 
 class Mutex {
@@ -14,5 +16,28 @@ class Mutex {
   inline void Lock() { m.lock(); }
   inline void Unlock() { m.unlock(); }
 };
+
+#else
+
+#include <pthread.h>
+
+class Mutex {
+ private:
+  pthread_mutex_t mutex_{};
+ public:
+  inline Mutex() {
+    mutex_ = PTHREAD_MUTEX_INITIALIZER;
+  }
+
+  inline void Lock() {
+    pthread_mutex_lock(&mutex_);
+  }
+
+  inline void Unlock() {
+    pthread_mutex_unlock(&mutex_);
+  }
+};
+
+#endif
 
 #endif //UTILITY_INCLUDE_UTILITY_MUTEX_H_
